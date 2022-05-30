271bbfa84b07f8779ad5d33aff7fb96e2cca7ab8
johirhaquedipok <johirhaquedipok2@gmail.com>
2022-05-30 07:45:01 +0600

    conditinoal chaining added for api for reviewdetails component

--- src/Components/ReviewDetails/ReviewDetails.js
+++ src/Components/ReviewDetails/ReviewDetails.js
@@ -8,10 +8,11 @@ const ReviewDetails = () => {
   const [reviews] = useReview(
     `https://jsonplaceholder.typicode.com/posts/${user_id}`
   );
+
   return (
-    <Card className="text-center">
+    <Card className="text-center my-5">
       <Card.Body>
-        <Card.Title>{reviews.title.slice(0, 10)}</Card.Title>
+        <Card.Title>{reviews.title?.slice(0, 10)}</Card.Title>
         <Card.Text>{reviews.body}</Card.Text>
       </Card.Body>
       <Card.Footer className="text-muted">Based on user params</Card.Footer>
