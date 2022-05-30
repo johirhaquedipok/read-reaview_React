0dd343fea50990b27accd05a2861ff79c53803b5
johirhaquedipok <johirhaquedipok2@gmail.com>
2022-05-30 19:55:35 +0600

    column and row and bit margin added to the dashboard

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,13 +1,18 @@
 import React from "react";
+import { Col, Row } from "react-bootstrap";
 import SimpleBarChart from "../SimpleBarChart/SimpleBarChart";
 import StackdChart from "../StackdChart/StackdChart";
 
 const Dashboard = () => {
   return (
-    <div>
-      <StackdChart></StackdChart>
-      <SimpleBarChart />
-    </div>
+    <Row>
+      <Col md={6} className=" mr-1">
+        <SimpleBarChart />
+      </Col>
+      <Col md={6} className=" ml-1">
+        <StackdChart></StackdChart>
+      </Col>
+    </Row>
   );
 };
 
